87cd81b9bca4d942c3a5e30a430e4ad3039f21d8
Seeru-crypto <puupuha@gmail.com>
2021-01-10 10:17:22 +0200

    push

--- Booker/phpHarj/ex5.php
+++ Booker/phpHarj/ex5.php
@@ -2,3 +2,28 @@
 
 $numbers = [3, 2, 5, 6];
 
+makeString1($numbers);
+
+
+function makeString1 ($array){
+    $string=join(", ",$array);
+    //echo $string;
+    StringToArray1 ($string);
+
+}
+
+function makeString2($array){
+    $string ="";
+    foreach ($array as $value ){
+        $string=$string.", ".$value;
+    }
+    $string = substr ($string,1);
+    echo $string;
+}
+function StringToArray1 ($string) {
+    $array=(explode(" ",$string));
+    foreach ($array as $value){
+        echo $value;
+    }
+
+}
