eb374440dbcb2ff864502cfb33a366ca3680cb3d
Seeru-crypto <puupuha@gmail.com>
2021-01-16 19:21:09 +0200

    Started to develop API connection to DB

--- Booker/HTML_files/test.php
+++ Booker/HTML_files/test.php
@@ -1,14 +1,42 @@
 <?php 
-$filePath=include"$_SERVER[DOCUMENT_ROOT]/HTML_files/test.json";
-//$filePath='C:\Users\Red\Managment files\Veebitehnoloogiad_Repo\Booker\HTML_files\test.json';
+$filePath='C:\Users\Red\Managment files\Veebitehnoloogiad_Repo\Booker\HTML_files\test.json';
 $arr = array("", "", );
 
-$additionalData= FormatDataToBeWritten($arr);
-$Originaldata = Read4File($filePath);
 
-$Originaldata[]=$additionalData;
-Write2File ($Originaldata, $filePath);
 
+Data4API();
+
+//$additionalData= FormatDataToBeWritten($arr);
+//$Originaldata = Read4File($filePath);
+
+//$Originaldata[]=$additionalData;
+//Write2File ($Originaldata, $filePath);
+
+function Data4API (){
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.appery.io/rest/1/apiexpress/api/QueryFolder/EndGroup/1?apiKey=213e6072-009c-4bd3-98b2-6d075c9bef7f",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+    
+
+$response = json_decode($response, true); //because of true, it's in an array
+echo 'Online: '. $response['AuthorID']['FirstName'];
+
+
+}
 
 function FormatDataToBeWritten ($arr){
     $tags = array ("firstName", "LastName", "ID"); 
@@ -17,9 +45,6 @@ function Read4File ($filePath){
     $str_Data = file_get_contents($filePath);
     return (json_decode($str_Data, true));
 }
-
-
-
 function Write2File ($data, $filePath){
     $fh = fopen($filePath, 'w')
         or die("Error opening output file");
@@ -28,3 +53,6 @@ function Write2File ($data, $filePath){
     echo ("done");
   }
   
+
+
+
